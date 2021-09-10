e0ff8aa3e29c19156d5d4df98953c6332f60dcca
hamidayb <hayub.bese18seecs@seecs.edu.pk>
2021-09-11 00:53:55 +0500

    forget password api added

--- models/user.model.js
+++ models/user.model.js
@@ -2,6 +2,8 @@ const con = require('../config/mysql')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 var nodemailer = require('nodemailer')
+const { use } = require('../routes/user.route')
+require('colors')
 
 const userModel = {
   register: (user) =>
@@ -12,76 +14,110 @@ const userModel = {
       con.query(
         `select * from xana.users where email='${user.email}' LIMIT 1`,
         async (err, res) => {
-          if (res.length !== 0) {
-            return reject(new Error('Email already exists', err))
-          } else {
-            const hashedPassword = await bcrypt.hash(`${user.password}`, 10)
-            let sql
-            if (user.mobile) {
-              sql = `INSERT into xana.users (name, email, mobile, password, roleId_fk) values ('${user.name}','${user.email}','${user.mobile}','${hashedPassword}',1)`
+          if (res) {
+            if (res.length !== 0) {
+              return reject(new Error('Email already exists', err))
             } else {
-              sql = `INSERT into xana.users (name, email, roleId_fk, image) values ('${user.name}','${user.email}',1, '${user.image}')`
-            }
-            con.query(sql, (err, res) => {
-              if (res) {
-                console.log(res.affectedRows)
-                if (res.affectedRows > 0) {
-                  console.log('added')
-                  const emailToken = jwt.sign(
-                    {
-                      id: `${res.insertId}`,
-                      sub: user.email,
-                    },
-                    'xanaCoding',
-                    {
-                      expiresIn: '8h',
-                    }
-                  )
+              const hashedPassword = await bcrypt.hash(`${user.password}`, 10)
+              const sql = `INSERT into xana.users (name, email, mobile, password, roleId_fk) values ('${user.name}','${user.email}','${user.mobile}','${hashedPassword}',1)`
+              con.query(sql, (err, res) => {
+                if (res) {
+                  console.log(res.affectedRows)
+                  if (res.affectedRows > 0) {
+                    console.log('added')
+                    const emailToken = jwt.sign(
+                      {
+                        id: `${res.insertId}`,
+                        sub: user.email,
+                      },
+                      'xanaCoding',
+                      {
+                        expiresIn: '8h',
+                      }
+                    )
 
-                  const url = `http://localhost:5112/api/user/confirmation/${emailToken}`
-                  const transporter = nodemailer.createTransport({
-                    host: 'mail.codistan.pk',
-                    port: 465,
-                    secure: true, // true for 465, false for other ports
-                    auth: {
-                      user: 'malik.mubashir@codistan.org',
-                      pass: 'Mailk@Mubashir321',
-                    },
-                  })
-                  const mailOptions = {
-                    from: 'malik.mubashir@codistan.org', // sender address
-                    to: 'malikmubashir025@gmail.com', // list of receivers
-                    subject: 'Subject of your email', // Subject line
-                    html: `<p>${url}</p>`, // plain text body
-                  }
-                  transporter.sendMail(mailOptions, function (err, info) {
-                    if (err) {
-                      console.log(err)
-                      return reject(new Error('Something went wrong', err))
-                    } else {
-                      console.log('info: ' + info)
-                      return resolve(res)
+                    const url = `http://localhost:${process.env.PORT}/api/user/confirmation/${emailToken}`
+                    const transporter = nodemailer.createTransport({
+                      host: 'mail.codistan.pk',
+                      port: 465,
+                      secure: true, // true for 465, false for other ports
+                      auth: {
+                        user: 'malik.mubashir@codistan.org',
+                        pass: 'Mailk@Mubashir321',
+                      },
+                    })
+                    const mailOptions = {
+                      from: 'malik.mubashir@codistan.org', // sender address
+                      to: user.email, // list of receivers
+                      subject: 'Email verification', // Subject line
+                      html: `<p>${url}</p>`, // plain text body
                     }
-                  })
+                    transporter.sendMail(mailOptions, function (err, info) {
+                      if (err) {
+                        console.log(err)
+                        return reject(new Error('Something went wrong', err))
+                      } else {
+                        console.log('info: ' + info)
+                        return resolve(res)
+                      }
+                    })
+                  }
+                } else {
+                  console.log('err', err)
+                  return reject(new Error('Something went wrong', err))
                 }
-              } else {
-                console.log('err', err)
-                return reject(new Error('Something went wrong', err))
-              }
-            })
+              })
+            }
+          } else {
+            return reject(new Error('Something went wrong', err))
           }
         }
       )
     }),
 
+  registerGmail: async (user) => {
+    await new Promise((resolve, reject) => {
+      // Email duplication check
+      con.query(
+        `select * from xana.users where email='${user.email}' LIMIT 1`,
+        (err, res) => {
+          if (res) {
+            if (res.length !== 0) {
+              return reject('Email already exists')
+            } else {
+              const sql = `INSERT into xana.users (name, email, roleId_fk, image, confirmed) values ('${user.name}','${user.email}',1, '${user.image}', 1)`
+              con.query(sql, (err, res) => {
+                if (res) {
+                  console.log(`Affected Rows: ${res.affectedRows}`.yellow.bold)
+                  if (res.affectedRows > 0) {
+                    console.log(res)
+                    return resolve(res)
+                  } else {
+                    return reject(new Error('Error adding user'))
+                  }
+                } else {
+                  console.log('err', err)
+                  return reject(new Error('Something went wrong', err))
+                }
+              })
+            }
+          } else {
+            return reject(new Error('Something went wrong', err))
+          }
+        }
+      )
+    })
+  },
+
   verifyEmail: (user) =>
     new Promise((resolve, reject) => {
       con.query(
-        `Update xana.users set confirmed=1 where id='${user.id}'`,
+        `Update xana.users SET confirmed=1 where id='${user.id}'`,
         (err, res) => {
           if (res) {
             return resolve(res)
           } else {
+            console.log(err)
             return reject(new Error('Something went wrong', err))
           }
         }
@@ -92,19 +128,52 @@ const userModel = {
       con.query(
         `select * from xana.users where email='${user.email}' LIMIT 1`,
         async (err, res) => {
-          if (res.length !== 0) {
-            const { password: hashedPassword } = res[0]
-            const validPass = await bcrypt.compare(
-              user.password,
-              hashedPassword
-            )
-            if (validPass) {
-              return resolve({ data: res, valid: true })
+          if (res) {
+            if (res.length !== 0) {
+              if (res[0]['confirmed'] != 1) {
+                return reject(new Error('Email not verified'))
+              }
+              const { password: hashedPassword } = res[0]
+              const validPass = await bcrypt.compare(
+                user.password,
+                hashedPassword
+              )
+              if (validPass) {
+                return resolve({ data: res, valid: true })
+              } else {
+                return reject({
+                  data: err,
+                  valid: false,
+                  status: 500,
+                  message: 'Password is incorrect',
+                })
+              }
             } else {
-              return reject({ data: res, valid: false, status: 500 })
+              return reject({
+                data: err,
+                valid: false,
+                status: 404,
+                message: 'User is not registered.',
+              })
             }
           } else {
-            return reject(new Error('Email or Password is incorrect', err))
+            return reject(new Error('Something went wrong', err))
+          }
+        }
+      )
+    }),
+
+  loginGmail: (user) =>
+    new Promise((resolve, reject) => {
+      con.query(
+        `select * from xana.users where email='${user.email}' LIMIT 1`,
+        async (err, res) => {
+          if (res) {
+            return res.length !== 0
+              ? resolve({ data: res, valid: true })
+              : reject(new Error('Email not registered', err))
+          } else {
+            return reject(new Error('Something went wrong', err))
           }
         }
       )
@@ -112,17 +181,21 @@ const userModel = {
 
   getProfile: (userId) =>
     new Promise((resolve, reject) => {
-      con.query(
-        `select * from xana.users where id='${userId}' LIMIT 1`,
-        (err, res) => {
-          if (res.length !== 0) {
-            return resolve(res)
-          } else {
-            console.log('err', err)
-            return reject(new Error('Invalid User Id', err))
+      if (!isNaN(userId)) {
+        con.query(
+          `select * from xana.users where id='${userId}' LIMIT 1`,
+          (err, res) => {
+            if (res.length !== 0) {
+              return resolve(res)
+            } else {
+              console.log('err', err)
+              return reject(new Error('Invalid User Id', err))
+            }
           }
-        }
-      )
+        )
+      } else {
+        return reject(new Error('Invalid User Id'))
+      }
     }),
 
   updateProfile: (userId, userData) =>
@@ -141,24 +214,135 @@ const userModel = {
             const mobile = userData.mobile || null
             const image = userData.image || null
             const address = userData.address || null
-            const password = userData.password
-              ? await bcrypt.hash(`${userData.password}`, 10)
-              : null
+            const password =
+              userData.password || null
+                ? await bcrypt.hash(`${userData.password}`, 10)
+                : null
 
             const sql = `UPDATE xana.users SET name='${name}',mobile='${mobile}',password='${password}',image='${image}', address='${address}' WHERE id='${userId}'`
 
             con.query(sql, (err, res) => {
               if (res) {
-                // console.log(res.affectedRows)
+                console.log(`Affected Rows: ${res.affectedRows}`.yellow.bold)
                 if (res.affectedRows > 0) {
                   console.log('updated')
                   return resolve(res)
                 }
               } else {
-                // console.log('err', err)
+                console.log('err', err)
                 return reject(new Error('Something went wrong', err))
               }
             })
+          }
+        }
+      )
+    }),
+
+  sendForgotPasswordMail: async (user) =>
+    await new Promise((resolve, reject) => {
+      console.log('user email: ', user.email)
+
+      // Email duplication check
+      con.query(
+        `select * from xana.users where email='${user.email}' LIMIT 1`,
+        async (err, res) => {
+          if (res) {
+            if (res.length === 0) {
+              return reject(new Error('Email not registered'))
+            } else {
+              const userId = res[0]['id']
+              const emailToken = jwt.sign(
+                {
+                  id: `${res.insertId}`,
+                  sub: user.email,
+                },
+                process.env.JWT_KEY,
+                {
+                  expiresIn: '1h',
+                }
+              )
+              const url = `http://localhost:${process.env.PORT}/user/${userId}/reset-password/${emailToken}`
+              const transporter = nodemailer.createTransport({
+                host: 'mail.codistan.pk',
+                port: 465,
+                secure: true, // true for 465, false for other ports
+                auth: {
+                  user: 'malik.mubashir@codistan.org',
+                  pass: 'Mailk@Mubashir321',
+                },
+              })
+              const mailOptions = {
+                from: 'malik.mubashir@codistan.org', // sender address
+                to: user.email, // list of receivers
+                subject: 'Password reset Link', // Subject line
+                html: `<p>${url}</p>`, // plain text body
+              }
+              transporter.sendMail(mailOptions, function (err, info) {
+                if (err) {
+                  console.log(err)
+                  return reject(new Error('Something went wrong', err))
+                } else {
+                  con.query(
+                    `UPDATE xana.users SET token = '${emailToken}' WHERE id=${userId} `,
+                    (err, res) => {
+                      if (err) {
+                        console.log(err)
+                        return reject(new Error('Something went wrong'))
+                      } else {
+                        console.log('info: ' + info)
+                        return resolve(res)
+                      }
+                    }
+                  )
+                }
+              })
+            }
+          } else {
+            console.log(err)
+            return reject(new Error('Something went wrong', err))
+          }
+        }
+      )
+    }),
+
+  resetPassword: async (userId, token, password) =>
+    await new Promise((resolve, reject) => {
+      console.log(password)
+      con.query(
+        `select * from xana.users where id='${userId}' LIMIT 1`,
+        async (err, res) => {
+          if (res) {
+            console.log(`${res[0]['token']}`.green)
+            if (res.length === 0) {
+              return reject(new Error(`User with id ${userId} not found`))
+            } else {
+              if (token === res[0]['token']) {
+                const hashedPassword = await bcrypt.hash(password, 10)
+                con.query(
+                  `UPDATE xana.users SET password = '${hashedPassword}', token=null WHERE id=${userId}`,
+                  (err, res) => {
+                    if (res) {
+                      console.log(res)
+                      return resolve(res)
+                    } else {
+                      console.log(err)
+                      return reject(
+                        new Error(
+                          'Something went wrong while updating password',
+                          err
+                        )
+                      )
+                    }
+                  }
+                )
+              } else {
+                console.log('Token Failed')
+                return reject(new Error('Token Failed'))
+              }
+            }
+          } else {
+            console.log(err)
+            return reject(new Error('Something went wrong', err))
           }
         }
       )
--- routes/user.route.js
+++ routes/user.route.js
@@ -3,42 +3,7 @@ const userRouter = express.Router()
 const userModel = require('../models/user.model')
 // const sqlHelper = require('../helpers/sqlHeplers')
 const jwt = require('jsonwebtoken')
-const { body, validationResult } = require('express-validator')
-
-// POST /api/user/register/gmail
-userRouter.post(
-  '/register/gmail',
-  [
-    body('name').not().isEmpty(),
-    body('email').not().isEmpty(),
-    body('image').not().isEmpty(),
-  ],
-  (req, res) => {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-      return res.status(422).jsonp(errors.array())
-    } else {
-      userModel
-        .register(req.body)
-        .then((userObj) => {
-          res.json({
-            data: userObj,
-            success: true,
-            message: 'User added successfully',
-          })
-          console.log('register ::>> res', userObj)
-        })
-        .catch((err) => {
-          console.log('register ::>> err', err)
-          res.status(400).json({
-            data: err,
-            success: false,
-            message: err.message,
-          })
-        })
-    }
-  }
-)
+const { body, param, validationResult } = require('express-validator')
 
 // POST /api/user/register
 userRouter.post(
@@ -60,9 +25,44 @@ userRouter.post(
           res.json({
             data: userObj,
             success: true,
-            message: 'Admin inserted successfully',
+            message: 'User inserted successfully',
           })
           console.log('register ::>> res', userObj)
+        })
+        .catch((err) => {
+          console.log('register ::>> err', err)
+          // console.log(sqlHelper.consoleSQLException(err))
+          res.json({
+            data: err,
+            success: false,
+            message: err.message,
+          })
+        })
+    }
+  }
+)
+
+// POST /api/user/register
+userRouter.post(
+  '/register/gmail',
+  [
+    body('name').not().isEmpty(),
+    body('email').not().isEmpty(),
+    body('image').not().isEmpty(),
+  ],
+  (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(422).jsonp(errors.array())
+    } else {
+      userModel
+        .registerGmail(req.body)
+        .then((userObj) => {
+          res.json({
+            data: userObj,
+            success: true,
+            message: 'User inserted successfully',
+          })
         })
         .catch((err) => {
           console.log('register ::>> err', err)
@@ -80,18 +80,20 @@ userRouter.post(
 // GET /api/user/confirmation/:token
 userRouter.get('/confirmation/:token', async (req, res) => {
   try {
-    const user = jwt.verify(req.params.token, 'xanaCoding')
+    const user = jwt.verify(req.params.token, process.env.JWT_KEY)
     userModel
       .verifyEmail(user)
-      .then((res) => {
+      .then(() => {
         res.json({
+          success: true,
           message: 'User verified successfully',
         })
       })
       .catch((err) => {
         res.json({
           error: err,
-          message: 'User verified successfully',
+          success: false,
+          message: 'Something went wrong',
         })
       })
   } catch (err) {
@@ -111,7 +113,7 @@ userRouter.post(
       userModel
         .login(req.body)
         .then((userObj) => {
-          console.log('register ::>> res', userObj)
+          console.log('login ::>> res', userObj)
           res.json({
             data: userObj.data,
             success: true,
@@ -119,12 +121,12 @@ userRouter.post(
           })
         })
         .catch((err) => {
-          console.log('register ::>> err', err)
+          console.log('login ::>> err', err)
           if (err.valid === false) {
             console.log('false')
             res.json({
               success: false,
-              message: 'Password is incorrect',
+              message: err.message,
             })
           } else {
             res.json({
@@ -138,6 +140,42 @@ userRouter.post(
     }
   }
 )
+
+// POST /api/user/login/gmail
+userRouter.post('/login/gmail', [body('email').not().isEmpty()], (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(422).jsonp(errors.array())
+  } else {
+    userModel
+      .loginGmail(req.body)
+      .then((userObj) => {
+        console.log('login ::>> res', userObj)
+        res.json({
+          data: userObj.data,
+          success: true,
+          message: 'User logged in successfully',
+        })
+      })
+      .catch((err) => {
+        console.log('login ::>> err', err)
+        if (err.valid === false) {
+          console.log('false')
+          res.json({
+            success: false,
+            message: 'Email or Password is incorrect',
+          })
+        } else {
+          res.json({
+            data: err,
+            success: false,
+            // message: sqlHelper.consoleSQLException(err),
+            message: err.message,
+          })
+        }
+      })
+  }
+})
 
 // GET /api/user/profile/:id
 userRouter.get('/profile/:id', (req, res) => {
@@ -163,31 +201,93 @@ userRouter.get('/profile/:id', (req, res) => {
 })
 
 // PUT /api/user/profile/:id/edit
-userRouter.put('/profile/:id/edit', (req, res) => {
-  const errors = validationResult(req)
-  if (!errors.isEmpty()) {
-    return res.status(422).jsonp(errors.array())
-  } else {
-    userModel
-      .updateProfile(req.params.id, req.body)
-      .then((userObj) => {
-        res.json({
-          data: userObj,
-          success: true,
-          message: 'User updated successfully',
+userRouter.put(
+  '/profile/:id/edit',
+  [body('name').not().isEmpty()],
+  (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(422).jsonp(errors.array())
+    } else {
+      userModel
+        .updateProfile(req.params.id, req.body)
+        .then((userObj) => {
+          res.json({
+            data: userObj,
+            success: true,
+            message: 'User updated successfully',
+          })
+          console.log('UpdateProfile ::>> res', userObj)
         })
-        console.log('UpdateProfile ::>> res', userObj)
-      })
-      .catch((err) => {
-        console.log('UpdateProfile ::>> err', err)
-        // console.log(sqlHelper.consoleSQLException(err))
-        res.json({
-          data: err,
-          success: false,
-          message: err.message,
+        .catch((err) => {
+          console.log('UpdateProfile ::>> err', err)
+          // console.log(sqlHelper.consoleSQLException(err))
+          res.json({
+            data: err,
+            success: false,
+            message: err.message,
+          })
         })
-      })
+    }
   }
-})
+)
 
+userRouter.post(
+  '/forgot-password',
+  [body('email').not().isEmpty()],
+  (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(422).jsonp(errors.array())
+    } else {
+      userModel
+        .sendForgotPasswordMail(req.body)
+        .then((userObj) => {
+          res.json({
+            data: userObj,
+            success: true,
+            message: 'Password reset email sent successfully',
+          })
+          console.log('Password Reset ::>> res', userObj)
+        })
+        .catch((err) => {
+          console.log('Password Reset ::>> err', err)
+          // console.log(sqlHelper.consoleSQLException(err))
+          res.json({
+            data: err,
+            success: false,
+            message: err.message,
+          })
+        })
+    }
+  }
+)
+
+// POST /api/user/:id/reset-password
+userRouter.post(
+  '/:id/reset-password',
+  [body('password').not().isEmpty(), body('token').not().isEmpty()],
+  async (req, res) => {
+    try {
+      userModel
+        .resetPassword(req.params.id, req.body.token, req.body.password)
+        .then((userObj) => {
+          res.json({
+            data: userObj,
+            success: true,
+            message: 'Password reset',
+          })
+        })
+        .catch((err) => {
+          res.json({
+            success: false,
+            error: err,
+            message: err.message,
+          })
+        })
+    } catch (err) {
+      res.json({ success: false, error: err })
+    }
+  }
+)
 module.exports = userRouter
