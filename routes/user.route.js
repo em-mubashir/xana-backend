4da9e1e61e5c46bc12c5f9a127244caf9ffb0b01
Haseeb Khan <haseeb_f_khan@outlook.com>
2021-12-07 12:12:08 +0500

    updated

--- routes/user.route.js
+++ routes/user.route.js
@@ -61,7 +61,7 @@ userRouter.post("/getreporturl", (req, res) => {
  * @returns userObj
  * @type POST
  * @params firstName,lastName,password,email,mobile
- * @route [http://192.168.0.107/api/user/register]
+ * @route [http://192.168.18.62/api/user/register]
  */
 userRouter.post(
   "/register",
@@ -106,7 +106,7 @@ userRouter.post(
  * @returns userObj
  * @type POST
  * @params firstName,lastName, email
- * @route [http://192.168.0.107/api/user/register/gmail]
+ * @route [http://192.168.18.62/api/user/register/gmail]
  */
 userRouter.post(
   "/register/gmail",
@@ -147,7 +147,7 @@ userRouter.post(
  * Token Verification
  * @returns success
  * @type GET
- * @route [http://192.168.0.107/api/user/confirmation/:token]
+ * @route [http://192.168.18.62/api/user/confirmation/:token]
  */
 userRouter.get("/confirmation/:token", verifyToken, async (req, res) => {
   try {
@@ -178,7 +178,7 @@ userRouter.get("/confirmation/:token", verifyToken, async (req, res) => {
  * @returns userId, sessionId, accessToken, refreshToken
  * @type POST
  * @params password,email
- * @route [http://192.168.0.107/api/user/login]
+ * @route [http://192.168.18.62/api/user/login]
  */
 userRouter.post(
   "/login",
@@ -226,7 +226,7 @@ userRouter.post(
  * Logout
  * @returns success
  * @type POST
- * @route [http://192.168.0.107/api/user/logout]
+ * @route [http://192.168.18.62/api/user/logout]
  */
 userRouter.post("/logout", async (req, res) => {
   try {
@@ -252,7 +252,7 @@ userRouter.post("/logout", async (req, res) => {
  * Login with gmail
  * @returns userObj
  * @type POST
- * @route [http://192.168.0.107/api/user/login/gmail]
+ * @route [http://192.168.18.62/api/user/login/gmail]
  */
 userRouter.post("/login/gmail", [body("email").not().isEmpty()], (req, res) => {
   const errors = validationResult(req);
@@ -294,7 +294,7 @@ userRouter.post("/login/gmail", [body("email").not().isEmpty()], (req, res) => {
  * @returns userObj
  * @type get
  * @required access_token
- * @route [http://192.168.0.107/api/user/profile]
+ * @route [http://192.168.18.62/api/user/profile]
  */
 userRouter.get(
   "/profile",
@@ -330,7 +330,7 @@ userRouter.get(
  * @type put
  * @params first_name
  * @required access_token
- * @route [http://192.168.0.107/api/user/profile/edit]
+ * @route [http://192.168.18.62/api/user/profile/edit]
  */
 userRouter.put(
   "/profile/edit",
@@ -367,7 +367,7 @@ userRouter.put(
  * @type post
  * @params email
  * @required access_token
- * @route [http://192.168.0.107/api/user/forgot-password]
+ * @route [http://192.168.18.62/api/user/forgot-password]
  */
 userRouter.post(
   "/forgot-password",
@@ -403,7 +403,7 @@ userRouter.post(
  * Reset Password
  * @returns userId
  * @type get
- * @route [http://192.168.0.107/api/user/reset-password/:token]
+ * @route [http://192.168.18.62/api/user/reset-password/:token]
  */
 userRouter.get("/reset-password/:token", async (req, res) => {
   userModel
@@ -430,7 +430,7 @@ userRouter.get("/reset-password/:token", async (req, res) => {
  * @type post
  * @returns success
  * @params email
- * @route [http://192.168.0.107/api/user/resend-code]
+ * @route [http://192.168.18.62/api/user/resend-code]
  */
 userRouter.post("/resend-code", async (req, res) => {
   userModel
@@ -457,7 +457,7 @@ userRouter.post("/resend-code", async (req, res) => {
  * @type post
  * @returns accessToken, refreshToken
  * @params refreshToken
- * @route [http://192.168.0.107/api/user/refresh-token]
+ * @route [http://192.168.18.62/api/user/refresh-token]
  */
 userRouter.post("/refresh-token", async (req, res) => {
   try {
@@ -486,7 +486,7 @@ userRouter.post("/refresh-token", async (req, res) => {
  * @type put
  * @returns success
  * @params id,password
- * @route [http://192.168.0.107/api/user/update-password]
+ * @route [http://192.168.18.62/api/user/update-password]
  */
 userRouter.put("/update-password", async (req, res) => {
   userModel
