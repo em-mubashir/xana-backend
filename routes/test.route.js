02f56e89cbacb7699cd73eabf129da6ab43d0708
em-mubashir <malikmubashir025@gmail.com>
2021-12-09 05:18:27 +0000

    bug fixed in result API  of test

--- routes/test.route.js
+++ routes/test.route.js
@@ -1,28 +1,28 @@
-const express = require("express");
+const express = require('express');
 const testRouter = express.Router();
-const FormData = require("form-data");
-const fs = require("fs");
-const testModel = require("../models/test.model");
-const multer = require("multer");
-const path = require("path");
-const axios = require("axios");
-const { verifyToken } = require("../middlewares/auth.middleware");
+const FormData = require('form-data');
+const fs = require('fs');
+const testModel = require('../models/test.model');
+const multer = require('multer');
+const path = require('path');
+const axios = require('axios');
+const { verifyToken } = require('../middlewares/auth.middleware');
 
-const { body, validationResult, errors } = require("express-validator");
+const { body, validationResult, errors } = require('express-validator');
 // multer file store start
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, "./uploads/testImages/");
+    cb(null, './uploads/testImages/');
   },
   filename(req, file, cb) {
     cb(null, `test-${Date.now()}${file.originalname}`);
   },
 });
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
     cb(null, true);
   } else {
-    cb(new Error("file have different extension"), false);
+    cb(new Error('file have different extension'), false);
   }
 };
 const upload = multer({
@@ -42,14 +42,14 @@ const upload = multer({
  * @route [http://192.168.18.62/api/reports/user]
  */
 testRouter.post(
-  "/create-new",
+  '/create-new',
   verifyToken,
   [
-    body("testName").not().isEmpty(),
-    body("Manufacturer").not().isEmpty(),
-    body("Description").not().isEmpty(),
-    body("Performance").not().isEmpty(),
-    body("Authorisation").not().isEmpty(),
+    body('testName').not().isEmpty(),
+    body('Manufacturer').not().isEmpty(),
+    body('Description').not().isEmpty(),
+    body('Performance').not().isEmpty(),
+    body('Authorisation').not().isEmpty(),
   ],
   (req, res) => {
     const errors = validationResult(req);
@@ -62,11 +62,11 @@ testRouter.post(
           res.json({
             data: userObj,
             success: true,
-            message: "Test Saved Successfully",
+            message: 'Test Saved Successfully',
           });
         })
         .catch((err) => {
-          console.log("add new test ::>> err", err);
+          console.log('add new test ::>> err', err);
           // console.log(sqlHelper.consoleSQLException(err))
           res.json({
             data: err,
@@ -84,32 +84,32 @@ testRouter.post(
  * @required access_token
  * @route [http://192.168.18.62/api/reports/user]
  */
-testRouter.put("/upload-test-image", (req, res) => {
+testRouter.put('/upload-test-image', (req, res) => {
   console.log(req);
   console.log(req.body);
   testModel
     .saveImage(req.body.img, req.body.testId)
     .then((fileObj) => {
-      console.log("image stored ", fileObj);
+      console.log('image stored ', fileObj);
       res
         .status(200)
-        .send({ success: true, message: "Image uploaded successfully" });
+        .send({ success: true, message: 'Image uploaded successfully' });
     })
     .catch((err) => {
       res.status(500).send({ success: false, message: err });
     });
 });
 
-testRouter.put("/upload-test-video", (req, res) => {
+testRouter.put('/upload-test-video', (req, res) => {
   console.log(req);
   console.log(req.body);
   testModel
     .saveVideo(req.body.video, req.body.testId)
     .then((fileObj) => {
-      console.log("Video stored ", fileObj);
+      console.log('Video stored ', fileObj);
       res
         .status(200)
-        .send({ success: true, message: "Video uploaded successfully" });
+        .send({ success: true, message: 'Video uploaded successfully' });
     })
     .catch((err) => {
       res.status(500).send({ success: false, message: err });
@@ -123,56 +123,39 @@ testRouter.put("/upload-test-video", (req, res) => {
  * @required access_token
  * @route [http://192.168.18.14/api/test/result?id=1]
  */
-testRouter.get("/result", async (req, res) => {
-  console.log("req.query ::: ", req.query.id);
-  let image =
-    "http://192.168.18.62:5000/uploads/testImages/test-1636407520684_0_35.png";
+testRouter.get('/result', async (req, res) => {
+  console.log('req.query ::: ', req.query.id);
+
   try {
     const { qr_id, test_image } = await testModel.getUserTestImg(req.query.id);
-    console.log("res", qr_id, test_image);
-
-    // const { results = "True" } = await axios.post(
-    //   "http://192.168.18.62:5002/xana",
-    //   {
-    //     qr_id,
-    //     test_image,
-    //   }
-    // );
-    const results = "True";
-    console.log("results", results);
-    res
-      .status(200)
-      .send(await testModel.addReportResult(results, req.query.id));
-    // if (res) {
-    //   const obj = { id: res.qr_id, img: res.test_image };
-    //   axios
-    //     .post("http://192.168.18.62:5002/xana", obj)
-    //     .then((response) => {
-    //       console.log("response", response);
-    //     })
-    //     .catch((err) => {});
-    // }
+    console.log('res', qr_id, test_image);
+    var data = JSON.stringify({
+      qr_id: qr_id,
+      img: test_image,
+      test_id: req.query.id,
+    });
+    var config = {
+      method: 'post',
+      url: process.env.IMAGE_DECTECTION+'/get_test_result',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: data,
+    };
+    const results = await axios(config)
+      .then(async function (response) {
+        console.log(JSON.stringify(response.data));
+        const { results } = response.data;
+        if (results) {
+          await testModel.addReportResult(results, req.query.id);
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   } catch (e) {
-    console.log("error", e);
+    console.log('error', e);
   }
-  // image = image.replace("http://192.168.18.62:5000/", "");
-  // console.log("image ::: ", image);
-  // const imagePath = path.join(__dirname, `../${image}`);
-  // console.log(imagePath, "imagePath");
-  // const data = fs.readFileSync(imagePath);
-  // console.log(data, "data");
-  // const fd = new FormData();
-  // fd.append("id", req.query.id);
-
-  // fd.append("img", image);
-  // axios
-  //   .post("http://192.168.18.62:5002/xana", fd)
-  //   .then((res) => {
-  //     console.log("res ::: ", res);
-  //   })
-  //   .catch((err) => {
-  //     console.log("err::: ", err);
-  //   });
 });
 
 module.exports = testRouter;
